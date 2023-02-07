ca4cae94e08da1a99d5d242644e89bfca03ea6aa
Nyarko Betrand <Betrand37@gmail.com>
2023-02-06 20:47:43 -0500

    first commit

--- MAPD724-W2023-ICE-2A/GameViewController.swift
+++ MAPD724-W2023-ICE-2A/GameViewController.swift
@@ -10,56 +10,102 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var ScoreLabel: UILabel!
     
+    @IBOutlet weak var StartLabel: UILabel!
+    var currentScene: GKScene?
+    @IBOutlet weak var EndLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        print(self.view.frame.size.width)
-//        print(self.view.frame.size.height)
-        
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-     
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                sceneNode.scaleMode = .aspectFill
-                sceneNode.size.width = 800
-                sceneNode.size.height = 1800
-                
-                if let view = self.view as! SKView?
-                {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                }
-                
-                
-            }
-        }
-        
-        CollisionManager.gameViewController = self
-               ScoreManager.Score = 0
-               ScoreManager.Lives = 5
-               updateLivesLabel()
-               updateScoreLabel()
-    }
+    @IBOutlet weak var StartButton: UIButton!
+    
+    @IBOutlet weak var RestartButton: UIButton!
+    
+    override func viewDidLoad()
+      {
+          super.viewDidLoad()
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-            return .portrait
-    }
+ //         presentEndScene()
+          
+          presentStartScene()
+          
+          // Initialize the Lives and Score
+          CollisionManager.gameViewController = self
+         
+      }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
+      override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+      {
+          return .portrait
+      }
+
+      override var prefersStatusBarHidden: Bool
+      {
+          return true
+      }
+      
+      func updateScoreLabel()
+      {
+          ScoreLabel.text = "Score: \(ScoreManager.Score)"
+      }
+      
+      func updateLivesLabel()
+      {
+          LifeLabel.text = "Lives: \(ScoreManager.Lives)"
+      }
+      
+      func setScene(sceneName: String) -> Void
+      {
+          currentScene = GKScene(fileNamed: sceneName)
+          
+          if let scene = currentScene!.rootNode as! SKScene?
+          {
+              scene.scaleMode = .aspectFill
+              
+              if let view = self.view as! SKView?
+              {
+                  view.presentScene(scene)
+                  view.ignoresSiblingOrder = true
+              }
+          }
+          
+      }
+    func presentStartScene(){
+        ScoreLabel.isHidden = true
+        LifeLabel.isHidden = true
+        StartButton.isHidden = false
+        EndLabel.isHidden = true
+        RestartButton.isHidden = true
+        setScene(sceneName: "StartScene")
+    }
+    func presentEndScene(){
+        RestartButton.isHidden = false
+        EndLabel.isHidden = false
+        ScoreLabel.isHidden = true
+        LifeLabel.isHidden = true
+        setScene(sceneName: "EndScene")
     }
     
-    func updateScoreLabel()
-    {
-        ScoreLabel.text = "Score: \(ScoreManager.Score)"
+    @IBAction func startButton_Pressed(_ sender: UIButton) {
+        StartButton.isHidden = true
+        StartLabel.isHidden = true
+        ScoreLabel.isHidden = false
+        LifeLabel.isHidden = false
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
+        setScene(sceneName: "GameScene")
     }
     
-    func updateLivesLabel()
-    {
-        LifeLabel.text = "Lives: \(ScoreManager.Lives)"
+    
+    @IBAction func restartButton_Pressed(_ sender: UIButton) {
+        RestartButton.isHidden = true
+        EndLabel.isHidden = true
+        ScoreLabel.isHidden = false
+        LifeLabel.isHidden = false
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
+        setScene(sceneName: "GameScene")
     }
+    
 }
--- MAPD724-W2023-ICE-2A/Managers/CollisionManager.swift
+++ MAPD724-W2023-ICE-2A/Managers/CollisionManager.swift
@@ -35,11 +35,18 @@ class CollisionManager
                     ScoreManager.Score += 100
                     gameViewController?.updateScoreLabel()
                     scene.run(SKAction.playSoundFileNamed("yay", waitForCompletion: false))
+                    if(ScoreManager.Score % 500 == 0){
+                        ScoreManager.Lives += 1
+                        gameViewController?.updateLivesLabel()
+                    }
                     break
                 case "cloud":
                     ScoreManager.Lives -= 1
                     gameViewController?.updateLivesLabel()
                     scene.run(SKAction.playSoundFileNamed("thunder", waitForCompletion: false))
+                    if(ScoreManager.Lives < 1){
+                        gameViewController?.presentEndScene()
+                    }
                     break
                 default:
                     break
